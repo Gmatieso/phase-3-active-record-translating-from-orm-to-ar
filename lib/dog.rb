f7d1da58c82b8588d61a4ca0348c86775f9889ad
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-11-05 11:01:14 +0300

    Inheriting from Active Record

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,8 @@
-class Dog
+#Inheriting from Active Record 
+
+class Dog < ActiveRecord::Base
 end
+
+
+#in the past, you had to write out each method individually to pass each test. Now with Active Record the hard work is done .
+#we just need to inherit from Active Record::Base method 
